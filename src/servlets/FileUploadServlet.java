7229c9b9c7f21ad3d83406ef91b42c5e47193b45
Albert Balbastre-Morte <bulbastre@gmail.com>
2017-11-15 17:39:37 +0100

    servlet sin errores de compilación

--- src/servlets/FileUploadServlet.java
+++ src/servlets/FileUploadServlet.java
@@ -1,3 +1,23 @@
+package servlets;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
 @WebServlet(name = "FileUploadServlet", urlPatterns = {"/upload"})
 @MultipartConfig
 public class FileUploadServlet extends HttpServlet {
@@ -64,3 +84,5 @@ public class FileUploadServlet extends HttpServlet {
         }
         return null;
     }
+    
+}
